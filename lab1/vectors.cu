b5ea014d56c7ccc5fd20fc76beafd45d65da6462
Yichen <49024218+greyhamechuck@users.noreply.github.com>
2025-10-01 18:18:16 -0400

    1

--- lab1/vectors.cu
+++ lab1/vectors.cu
@@ -79,79 +79,83 @@ int main(int argc, char *argv[]){
 	//The GPU part
 	
     // Create CUDA events for accurate timing
-    cudaEvent_t gpu_start, gpu_stop;
-    float gpu_elapsed_time;
-    cudaEventCreate(&gpu_start);
-    cudaEventCreate(&gpu_stop);
+		cudaEvent_t gpu_start, gpu_stop;
+		float gpu_elapsed_time;
+		cudaEventCreate(&gpu_start);
+		cudaEventCreate(&gpu_stop);
 
 	/* TODO: in this part you need to do the following:
 		1. allocate ad, bd, and cd in the device
 		2. send a, b, and c to the device  
 	*/
-    cudaMalloc((void **)&ad, size);
-    cudaMalloc((void **)&bd, size);
-    cudaMalloc((void **)&cd, size);
+		cudaMalloc((void **)&ad, size);
+		cudaMalloc((void **)&bd, size);
+		cudaMalloc((void **)&cd, size);
 
     // Record the start event
-    cudaEventRecord(gpu_start);
+		cudaEventRecord(gpu_start);
 
-    cudaMemcpy(ad, a, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(bd, b, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(cd, c, size, cudaMemcpyHostToDevice);
+		cudaMemcpy(ad, a, size, cudaMemcpyHostToDevice);
+		cudaMemcpy(bd, b, size, cudaMemcpyHostToDevice);
+		cudaMemcpy(cd, c, size, cudaMemcpyHostToDevice);
 		
 	/* TODO: 	
 		3. write the kernel, call it: vecGPU
 		4. call the kernel (the kernel itself will be written at the comment at the end of this file), 
 		   you need to decide about the number of threads, blocks, etc and their geometry.
 	*/
-    vecGPU<<<BLOCK_NUM, THREADS_NUM>>>(ad, bd, cd, n);
+		vecGPU<<<BLOCK_NUM, THREADS_NUM>>>(ad, bd, cd, n);
 		
 	/* TODO: 
 		5. bring the cd array back from the device and store it in c array (declared earlier in main)
 		6. free ad, bd, and cd
 	*/
-	cudaMemcpy(c, cd, size, cudaMemcpyDeviceToHost);
+		cudaMemcpy(c, cd, size, cudaMemcpyDeviceToHost);
 
     // Record the stop event and synchronize
-    cudaEventRecord(gpu_stop);
-    cudaEventSynchronize(gpu_stop);
+		cudaEventRecord(gpu_stop);
+		cudaEventSynchronize(gpu_stop);
 
     // Calculate the elapsed time
-    cudaEventElapsedTime(&gpu_elapsed_time, gpu_start, gpu_stop);
+		cudaEventElapsedTime(&gpu_elapsed_time, gpu_start, gpu_stop);
 
-    cudaFree(ad);
-    cudaFree(bd);
-    cudaFree(cd);
+		cudaFree(ad);
+		cudaFree(bd);
+		cudaFree(cd);
 	
-	printf("Total time taken by the GPU part = %f seconds\n", gpu_elapsed_time / 1000.0f); // Time is in ms
+		printf("Total time taken by the GPU part = %f seconds\n", gpu_elapsed_time / 1000.0f); // Time is in ms
 	/****************** The end of the GPU part: Do not modify anything in main() below this line  ************/
 	
 	//checking the correctness of the GPU part
-	for(i = 0; i < n; i++)
-	  if( fabs(temp[i] - c[i]) >= 0.009) //compare up to the second degit in floating point
-		printf("Element %d in the result array does not match the sequential version\n", i);
+		for(i = 0; i < n; i++)
+		if( fabs(temp[i] - c[i]) >= 0.009) //compare up to the second degit in floating point
+			printf("Element %d in the result array does not match the sequential version\n", i);
+			
+		// Free the arrays in the host
+		free(a); free(b); free(c); free(temp);
 		
-	// Free the arrays in the host
-	free(a); free(b); free(c); free(temp);
-    
-    // Destroy the events
-    cudaEventDestroy(gpu_start);
-    cudaEventDestroy(gpu_stop);
+		// Destroy the events
+		cudaEventDestroy(gpu_start);
+		cudaEventDestroy(gpu_stop);
 
-	return 0;
+		return 0;
 }
 
 
 /**** TODO: Write the kernel itself below this line *****/
-__global__ void vecGPU(float *ad, float *bd, float *cd, int n)
-{
-    // Calculate the global thread ID
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
-    // Calculate the total number of threads in the grid
-    int stride = gridDim.x * blockDim.x;
 
-    // Use a grid-stride loop to process all elements
-    for (int i = index; i < n; i += stride) {
-        cd[i] += ad[i] * bd[i];
-    }
+__global__
+void vecGPU(float* ad, float* bd, float* cd, int n)
+{
+    int totalNumOfThreads = blockDim.x * gridDim.x;
+	int i, j, index;
+	j = threadIdx.x + blockDim.x * blockIdx.x;
+	for (i = 0; i <= n/totalNumOfThreads; i++)
+	{
+		index = i * totalNumOfThreads + j;
+		if(index<n)
+		{
+			cd[index] += ad[index] * bd[index];
+		}
+	}
 }
