3fae975e800e8f9388155fe4d4edfec10a9e7c48
Yichen <49024218+greyhamechuck@users.noreply.github.com>
2025-10-01 21:54:37 -0400

    Update vectors.cu

--- lab1/vectors.cu
+++ lab1/vectors.cu
@@ -3,83 +3,77 @@
 #include <time.h>
 #include <cuda.h>
 
+#define RANGE 17.78
 #define BLOCK_NUM 8
 #define BLOCK_SIZE 500
 
-#define RANGE 19.87
-
-/*** Declaration of the kernel function below this line ***/
-
+/*** TODO: insert the declaration of the kernel function below this line ***/
 __global__ void vecGPU(float *a, float *b, float *c, int n, int stride);
+
 
 /**** end of the kernel declaration ***/
 
-int main(int argc, char *argv[])
-{
 
-	int n = 0;									// number of elements in the arrays
-	int i;											// loop index
-	float *a, *b, *c;						// The arrays that will be processed in the host.
-	float *temp;								// array in host used in the sequential code.
-	float *ad, *bd, *cd;				// The arrays that will be processed in the device.
-	struct timespec start, end; // to meaure the time taken by a specific part of code
+int main(int argc, char *argv[]){
 
-	if (argc != 2)
-	{
+	int n = 0; //number of elements in the arrays
+	int i;  //loop index
+	float *a, *b, *c; // The arrays that will be processed in the host.
+	float *temp;  //array in host used in the sequential code.
+	float *ad, *bd, *cd; //The arrays that will be processed in the device.
+	clock_t start, end; // to meaure the time taken by a specific part of code
+	
+	if(argc != 2){
 		printf("usage:  ./vectorprog n\n");
 		printf("n = number of elements in each vector\n");
 		exit(1);
-	}
-
+		}
+		
 	n = atoi(argv[1]);
 	printf("Each vector will have %d elements\n", n);
-
-	// Allocating the arrays in the host
-
-	if (!(a = (float *)malloc(n * sizeof(float))))
+	
+	
+	//Allocating the arrays in the host
+	
+	if( !(a = (float *)malloc(n*sizeof(float))) )
 	{
-		printf("Error allocating array a\n");
-		exit(1);
+	   printf("Error allocating array a\n");
+	   exit(1);
 	}
-
-	if (!(b = (float *)malloc(n * sizeof(float))))
+	
+	if( !(b = (float *)malloc(n*sizeof(float))) )
 	{
-		printf("Error allocating array b\n");
-		exit(1);
+	   printf("Error allocating array b\n");
+	   exit(1);
 	}
-
-	if (!(c = (float *)malloc(n * sizeof(float))))
+	
+	if( !(c = (float *)malloc(n*sizeof(float))) )
 	{
-		printf("Error allocating array c\n");
-		exit(1);
+	   printf("Error allocating array c\n");
+	   exit(1);
 	}
-
-	if (!(temp = (float *)malloc(n * sizeof(float))))
+	
+	if( !(temp = (float *)malloc(n*sizeof(float))) )
 	{
-		printf("Error allocating array temp\n");
-		exit(1);
+	   printf("Error allocating array temp\n");
+	   exit(1);
 	}
-
-	// Fill out the arrays with random numbers between 0 and RANGE;
+	
+	//Fill out the arrays with random numbers between 0 and RANGE;
 	srand((unsigned int)time(NULL));
-	for (i = 0; i < n; i++)
-	{
-		a[i] = ((float)rand() / (float)(RAND_MAX)) * RANGE;
-		b[i] = ((float)rand() / (float)(RAND_MAX)) * RANGE;
-		c[i] = ((float)rand() / (float)(RAND_MAX)) * RANGE;
-		temp[i] = c[i]; // temp is just another copy of C
+	for (i = 0; i < n;  i++){
+        a[i] = ((float)rand()/(float)(RAND_MAX)) * RANGE;
+		b[i] = ((float)rand()/(float)(RAND_MAX)) * RANGE;
+		c[i] = ((float)rand()/(float)(RAND_MAX)) * RANGE;
+		temp[i] = c[i]; //temp is just another copy of C
 	}
-
-	// The sequential part
-
-	clock_gettime(CLOCK_REALTIME, &start);
-
-	for (i = 0; i < n; i++)
+	
+    //The sequential part
+	start = clock();
+	for(i = 0; i < n; i++)
 		temp[i] += a[i] * b[i];
-
-	clock_gettime(CLOCK_REALTIME, &end);
-
-	printf("Total time taken by the sequential part = %lf\n", (double)(end.tv_sec - start.tv_sec) + (double)(end.tv_nsec - start.tv_nsec) / 1000000000);
+	end = clock();
+	printf("Total time taken by the sequential part = %lf\n", (double)(end - start) / CLOCKS_PER_SEC);
 
 	/******************  The start GPU part: Do not modify anything in main() above this line  ************/
 	// The GPU part
@@ -116,24 +110,23 @@ int main(int argc, char *argv[])
 	cudaFree(cd);
 
 	printf("Total time taken by the GPU part = %lf\n", (double)(end.tv_sec - start.tv_sec) + (double)(end.tv_nsec - start.tv_nsec) / 1000000000);
+	
+	
 	/******************  The end of the GPU part: Do not modify anything in main() below this line  ************/
-
-	// Checking the correctness of the GPU part
-	for (i = 0; i < n; i++)
-		if (fabs(temp[i] - c[i]) >= 0.009) // compare up to the second degit in floating point
-			printf("Element %d in the result array does not match the sequential version\n", i);
-
+	
+	//checking the correctness of the GPU part
+	for(i = 0; i < n; i++)
+	  if( fabs(temp[i] - c[i]) >= 0.009) //compare up to the second degit in floating point
+		printf("Element %d in the result array does not match the sequential version\n", i);
+		
 	// Free the arrays in the host
-	free(a);
-	free(b);
-	free(c);
-	free(temp);
+	free(a); free(b); free(c); free(temp);
 
 	return 0;
 }
 
-/**** Write the kernel itself below this line *****/
 
+/**** TODO: Write the kernel itself below this line *****/
 __global__ void vecGPU(float *a, float *b, float *c, int n, int stride)
 {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
